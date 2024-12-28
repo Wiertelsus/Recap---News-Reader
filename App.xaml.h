82e1d9405863c6ca51dcdf3f11b8f31687fc6872
kuba <kuba@DESKTOP-NES7QLP>
2024-12-28 14:53:37 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::Recap___News_Reader::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+
+    private:
+        winrt::Microsoft::UI::Xaml::Window window{ nullptr };
+    };
+}
